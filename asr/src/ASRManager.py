1fb2fb00c982ac0d7daecb34648bdf1a42fd0957
ryanlohyr <ryanloh29@gmail.com>
2024-05-20 11:50:40 +0000

    Add base version of asr

--- asr/src/ASRManager.py
+++ asr/src/ASRManager.py
@@ -1,8 +1,22 @@
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
+import torch
+import torchaudio
+import io
+
 class ASRManager:
     def __init__(self):
-        # initialize the model here
-        pass
+        self.model_name = "facebook/wav2vec2-base-960h"
+        self.processor = Wav2Vec2Processor.from_pretrained(self.model_name)
+        self.model = Wav2Vec2ForCTC.from_pretrained(self.model_name).to("cuda")
 
     def transcribe(self, audio_bytes: bytes) -> str:
-        # perform ASR transcription
-        return ""
+        waveform, sample_rate = torchaudio.load(io.BytesIO(audio_bytes))
+        inputs = self.processor(waveform, sampling_rate=sample_rate, return_tensors="pt", padding=True).to("cuda")
+
+        with torch.no_grad():
+            logits = self.model(inputs.input_values).logits
+
+        predicted_ids = torch.argmax(logits, dim=-1)
+        transcription = self.processor.batch_decode(predicted_ids)
+        return transcription[0]
+
